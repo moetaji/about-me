4a7cc6c2eb49e05f42cc63d012d504ca1bcbb557
moetaji <mohmmad.altaji@gmail.com>
2021-03-30 23:58:57 +0300

    updat-about-me

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # about-me
 LAB - 02
 about-me 
-I build this to let user know me better who i'm
+I build this to let users know me better who i'm
 
 Author: mohmmad ibrahem subhi altajee
 Links and Resources:
@@ -13,3 +13,11 @@ answers for my qousions
 - i love drinking hot drink more than cold one 
 - i'am a light smoker 
 - i'm tall persone 183m
+------------------------------
+<h1>updat for lab 03</h1>
+added Q 6 and Q 7
+q 6 to qusse my lucky number
+the answer was number 7
+q 7 qussing my type of music
+the right answer rock , pop,rap
+
--- js/app.js
+++ js/app.js
@@ -1,51 +1,98 @@
-'use strict'
- let welcomeMsg
-  welcomeMsg=prompt('hello what is your name')
-   //console.log(welcomeMsg);
-    alert('hello '+welcomeMsg);
-    let age
-    
-       age=prompt('do you think im 25 years old yes/no ')
-       age=age.toLocaleLowerCase();
-        //console.log(age);
-          if (age==='yes'||age==='y') {
-           //console.log(age);
-              alert('you are correct');    
-            }
-            else{
-             alert("sorry y are wrong")
-                } 
-                 let favortDrink
-                  
-                   favortDrink=prompt('do you think i like hot drinks yes/no')
-                    favortDrink=favortDrink.toLocaleLowerCase();
-                      //console.log(favortDrink);
-                       if (favortDrink==='yes'||favortDrink==='y') {
-                         alert('you are right i do love hot drinks')
-    
-                    }                      
-                        else {
-                         alert('unfortunately i do love hot drinks')
-                         }
-                          let smoker
-                            smoker=prompt('do you think i smoke yes/no')
-                             smoker=smoker.toLocaleLowerCase();
-                              if (smoker==='yes'||smoker==='y') {
-                               alert(' yes i do smoke')
-                              }  
-                               else {
-                                alert('i do smoke sometime')
+    "use strict";
 
-                             }
-                              let tall
-                               tall=prompt('do u think im tall yes/no')
-                                if (tall==='yes'||tall==='y') {
-                                 alert('that is right im tall person')
-                                
-                                }
-                                else{
-                                 alert('well iam tall person')
-                               }
-                                alert("thank you for your time "+welcomeMsg)
-       
-  
+    let welcomeMsg
+    welcomeMsg = prompt('hello what is your name')
+    //console.log(welcomeMsg);
+    alert('hello ' + welcomeMsg);
+    let score=0;
+    let age
+    age = prompt('do you think im 25 years old yes/no ')
+    age = age.toLocaleLowerCase();
+    //console.log(age);
+    if (age === 'yes' || age === 'y') {
+    //console.log(age);
+    alert('you are correct');
+    score=+1
+    
+    }
+    else {
+    alert("sorry y are wrong")
+    }
+    let favortDrink
+
+      favortDrink = prompt('do you think i like hot drinks yes/no')
+      favortDrink = favortDrink.toLocaleLowerCase();
+      //console.log(favortDrink);
+      if (favortDrink === 'yes' || favortDrink === 'y') {
+      alert('you are right i do love hot drinks')
+      score=+1
+    }
+    else {
+    alert('unfortunately i do love hot drinks')
+    }
+    let smoker
+    smoker = prompt('do you think i smoke yes/no')
+    smoker = smoker.toLocaleLowerCase();
+    if (smoker === 'yes' || smoker === 'y') {
+      alert(' yes i do smoke')
+      score=+1
+    }
+    else {
+    alert('i do smoke sometime')
+
+    }
+    let tall
+    tall = prompt('do u think im tall yes/no')
+    if (tall === 'yes' || tall === 'y') {
+      alert('that is right im tall person')
+      score=+1
+
+    }
+    else {
+    alert('well iam tall person')
+    }
+    
+    
+    let luckyNumber = prompt('can you gusse my number from 1 to 10')
+    for (let i = 0; i < 4; i++) {
+      if (luckyNumber <7)
+      {alert('to low')
+      luckyNumber=prompt('try again')
+    }
+    else if(luckyNumber >7){
+      alert('to high')
+      luckyNumber=prompt('try again')
+    }
+    else if(luckyNumber==7){
+      score=+
+      alert('you gusse it')
+    }
+  }
+    
+    let music
+    let list =['rock','pop','rap'];
+    for(let i=0;i<6;){
+      music=prompt('can you gusse what type of music i listen to?');
+      score=+1
+      let correct=0;
+    for(let t=0;t<list.length;t++){
+      if(list[t].toLowerCase()===music.toLowerCase()){
+        alert('that is right');
+        correct=1;
+        break;
+      }
+    }
+    if (correct===1){
+      break;
+    }
+    else if(!music.toString()){
+      alert('type somthing');}
+      else{
+        alert('try another time');
+      }
+      alert('attemt number'+ ++i)
+      
+    }
+    alert('correct answers '+list);
+    alert('result= '+score +'of 7' )
+    alert("thank you for your time " + welcomeMsg);
